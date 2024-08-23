6bb33ee89f144efab6bad5e7c186243de842ee95
farrasrifqi <farrasrifqi25@gmail.com>
2024-08-23 20:41:08 +0700

    Menambahkan file number, menjelaskan fungsi tipe data int dan double dan menjelaskan fungsi num

---
+++ bin/number.dart
@@ -0,0 +1,13 @@
+void main() {
+  // int dan double
+  int age = 19; // Tipe data bilangan bulat.
+  double pi = 3.14; // Tipe data bilangan desimal.
+  print(age);
+  print(pi);
+
+  // num = Bisa menerima nilai int dan double.
+  num number = 20;
+  print(number);
+  number = 20.1;
+  print(number);
+}
