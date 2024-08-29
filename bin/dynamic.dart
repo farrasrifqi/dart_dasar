cb7db30ea9acefecf6a985d10fb80a2ae5ffd4c4
farrasrifqi <farrasrifqi25@gmail.com>
2024-08-30 00:25:28 +0700

    Menambahkan file dynamic dan menjelaskan fungsinya

---
+++ bin/dynamic.dart
@@ -0,0 +1,9 @@
+void main() {
+  //* Tipe data yang digunakan jika kita ingin membuat variable yang bisa menampung semua jenis tipe data
+  dynamic random = "Halo";
+  print(random);
+  random = 10;
+  print(random);
+  random = true;
+  print(random);
+}
