f8a9ce2c0b6c4b72fa6b954ccb0207a0242b6d1f
farrasrifqi <farrasrifqi25@gmail.com>
2024-08-29 14:48:20 +0700

    Menambahkan file boolean dan menjelaskan fungsi boolean

---
+++ bin/boolean.dart
@@ -0,0 +1,7 @@
+void main() {
+  //* Boolean adalah tipe data yang hanya memiliki 2 nilai, yaitu true and false
+  bool isLoading = true;
+  print(isLoading);
+  isLoading = false;
+  print(isLoading);
+}
